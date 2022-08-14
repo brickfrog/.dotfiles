179cce3e0772e7180ed6e1901934ad8ab6a35fd9
Justin Malloy <justin@justin.vc>
2022-08-14 10:42:44 -0400

    misc changes / configs

--- config/awesome/theming.sh
+++ config/awesome/theming.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 ## wal colors & program theming
-wal -i ~/Pictures/Wallpapers/cyberpunk-highlife.jpg 
-source "${HOME}/.cache/wal/colors.sh" 
-xrdb ~/.cache/wal/colors.Xresources 
+wal -n -i ~/Pictures/Wallpapers/cyberpunk-high*
+feh --bg-fill "$(< "${HOME}/.cache/wal/wal")"  
+xrdb -load ~/.cache/wal/colors.Xresources 
 cp ~/.cache/wal/colors.Xresources ~/.Xresources
-/usr/bin/wpg -s "$wallpaper" -n
+wpg -n -s "$(< "${HOME}/.cache/wal/wal")"
 
-# Individual app theming
-# /usr/bin/pywalfox start &
+# wal program theming
 /usr/bin/pywalfox update &
 /usr/bin/wal-telegram --wal &
 /usr/bin/spicetify apply -q -n &
-
-feh --bg-fill "$wallpaper"
